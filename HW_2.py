64f7e452cedf47207ff98bcad124a21ae6a92d11
Sergey <santexnik.86@mail.ru>
2022-09-08 13:57:50 +0300

    Решение задачи 3

--- HW_2.py
+++ HW_2.py
@@ -26,9 +26,16 @@
 
 # 3 Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
 
-n = int(input('Введите целое число = '))
-sum = 0
-for i in range(1, n + 1):
-    print(i, ')', (1 + 1/i)**i)
-    sum = sum + ((1 + 1/i)**i)
-print('\nСумма: ', sum, '\n')
+# n = int(input('Введите целое число = '))
+# sum = 0
+# for i in range(1, n + 1):
+#     print(i, ')', (1 + 1/i)**i)
+#     sum = sum + ((1 + 1/i)**i)
+# print('\nСумма: ', sum, '\n')
+
+# 5 Реализуйте алгоритм перемешивания списка.
+
+import random
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+result = random.sample(list, len(list))
+print ('\n', str(list), ' получаем случайный список ', str(result), '\n')
