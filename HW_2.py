586833df9cc3f2d2110a98471fd892b039264644
Sergey <santexnik.86@mail.ru>
2022-09-08 13:57:15 +0300

    Решение задачи 3

--- HW_2.py
+++ HW_2.py
@@ -15,11 +15,20 @@
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-number = int(input('Введите целое число: '))
-print(type(number))
-factor = 1
-list = []
-for i in range(1, number + 1):
-    factor = factor * i
-    list.append(factor) 
-print(f"Произведения чисел от 1 до {number}:  {list}")
+# number = int(input('Введите целое число: '))
+# print(type(number))
+# factor = 1
+# list = []
+# for i in range(1, number + 1):
+#     factor = factor * i
+#     list.append(factor) 
+# print(f"Произведения чисел от 1 до {number}:  {list}")
+
+# 3 Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+
+n = int(input('Введите целое число = '))
+sum = 0
+for i in range(1, n + 1):
+    print(i, ')', (1 + 1/i)**i)
+    sum = sum + ((1 + 1/i)**i)
+print('\nСумма: ', sum, '\n')
